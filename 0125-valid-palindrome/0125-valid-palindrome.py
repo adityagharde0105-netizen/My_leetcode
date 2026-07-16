812605e082f58592c853adef3e004cbd55a30cfe
GHARDE ADITYA SANJAY <adityagharde0105@gmail.com>
2026-07-17 00:06:03 +0530

    Time: 11 ms (32.35%), Space: 19.5 MB (76.78%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,26 +1,21 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        palindrome = ""
 
+
+        clean = ""
+    
         for ch in s:
             if ch.isalnum():
-                palindrome += ch.lower()
-        
-        return palindrome == palindrome[::-1]
-
-
-'''left = 0
-        right = len(s)-1
-
-        while left > right:
-            if s[left] != s[right]:
+                clean += ch.lower()
+    
+        left = 0
+        right = len(clean) - 1
+    
+        while left < right:
+            if clean[left] != clean[right]:
                 return False
-
+    
             left += 1
             right -= 1
-
-        return True'''
-
-
-
-        
+    
+        return True
