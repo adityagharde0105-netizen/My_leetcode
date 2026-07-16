f8ede89117944d1ec823e4405b9bbe4c75ea31c8
GHARDE ADITYA SANJAY <adityagharde0105@gmail.com>
2026-07-16 23:58:45 +0530

    Time: 7 ms (81.2%), Space: 19.7 MB (52.79%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -7,4 +7,20 @@ class Solution:
                 palindrome += ch.lower()
         
         return palindrome == palindrome[::-1]
+
+
+'''left = 0
+        right = len(s)-1
+
+        while left > right:
+            if s[left] != s[right]:
+                return False
+
+            left += 1
+            right -= 1
+
+        return True'''
+
+
+
         
